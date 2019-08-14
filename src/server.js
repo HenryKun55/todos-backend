d0ec28da39e5bad0b2da1b698e726c04c0a1ff2e
Flávio <henriqueflavio55@gmail.com>
2019-08-14 20:58:48 -0300

    editing again

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const routes = require('./routes')
 
 const server = express()
 const port = 3333
 
-mongoose.connect('mongodb+srv://notes:notesdqv@cluster0-9maly.mongodb.net/notes?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://notes:notesdqv@cluster0-9maly.mongodb.net/notes?retryWrites=true', {
     useNewUrlParser: true
 })
 
+server.use(cors())
 server.use(express.json())
 server.use(express.urlencoded({extended: true}))
 server.use(routes)
