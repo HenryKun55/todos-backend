e4a2b1c5dd8020662e17c0125ac809cc3b61a64a
Flávio <henriqueflavio55@gmail.com>
2019-08-14 20:41:36 -0300

    Add cors

--- src/server.js
+++ src/server.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose')
 const routes = require('./routes')
 
 const server = express()
+const port = 3333
 
 mongoose.connect('mongodb+srv://notes:notesdqv@cluster0-9maly.mongodb.net/notes?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
 
 server.use(express.json())
+server.use(express.urlencoded({extended: true}))
 server.use(routes)
 
-server.listen(3333)
+server.listen(process.env.PORT || port)
